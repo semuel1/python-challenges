714285e194efa3f697aaa82fcacd2d37306fffc4
semuel1 <semuelst1@gmail.com>
2021-03-30 10:08:12 -0700

    three questions

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,15 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+
+user_input = input("What string do you want to reverse? ")
+
+def reverse(s):
+  result = ''
+  for char in s:
+    result = char + result
+
+  return result
+
+print(f"{user_input} reversed is: {reverse(user_input)}")
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,16 @@
+balance = 4000
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+
+def deposit():
+  return float(input("How much would you like to deposit?\n"))
+
+def withdraw():
+  return float(input("How much would you like to withdraw?\n"))
+
+def print_balance():
+  print(f"Your current balance is:\n{balance}")
+
+
+# print("Welcome to Chase bank.")
+# print("Have a nice day!")
 
